cad19d0589a7415855d7c3615b2800eba581ea45
ShivamGadhe <shivamgadhe55@gmail.com>
2025-03-07 10:32:46 +0530

    Delete HTML/app.routes.ts

--- HTML/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-
-export const routes: Routes = [{path:"register",component:RegisterComponent},{path:"login",component:LoginComponent}, { path:"",redirectTo:"login", pathMatch:"full"}];
